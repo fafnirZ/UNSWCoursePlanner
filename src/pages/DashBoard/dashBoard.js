7d5dba151c9a5d72d4ed1189d18c673278117e73
Zhilin Xie <jacky.xie@student.unsw.edu.au>
2021-03-23 01:18:54 +1100

    pages

--- src/pages/DashBoard/dashBoard.js
+++ src/pages/DashBoard/dashBoard.js
@@ -1,14 +1,21 @@
 import react, {useEffect} from 'react'
-
+import './dashBoard.css'
 
 function DashBoard(props) {
 
 
 	return (
-		<div>
-			this is the dash
+		<div className="Dashboard">
+			<div className="Header">
+				this is for the header
+			</div>
+			<div className="Body">
+				this is for body
+			</div>
+			<div className="Footer">
+				smelly footer
+			</div>
 		</div>
-
 	);
 }
 
