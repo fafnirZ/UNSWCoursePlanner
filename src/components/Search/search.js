8b1f11ae4cfc935bfb80c507c54799376c394a73
Zhilin Xie <jacky.xie@student.unsw.edu.au>
2021-03-27 18:39:04 +1100

    search n tree

---
+++ src/components/Search/search.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import './search.css'
+
+
+function Search() {
+
+	return (
+		<div className="SearchContainer">
+			<i>
+				Type here
+			</i>
+		</div>
+	);
+};
+
+export default Search;
---
+++ src/components/Tree/tree.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import './tree.css'
+
+function Tree(props) {
+
+	return (
+		<div className="Container">
+		</div>
+	);
+
+}
+
+export default Tree;
