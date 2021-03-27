68eff42b370954469d21068399c916ad06a998d8
Zhilin Xie <jacky.xie@student.unsw.edu.au>
2021-03-27 19:17:07 +1100

    aaa

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from './pages/HomePage/homePage.js';
 import DashBoard from './pages/DashBoard/dashBoard.js';
+import StudySpaceFinder from './pages/StudySpaceFinderPage/studyFinder.js'
 import './App.css';
 
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/dashboard" component={DashBoard} />
+        <Route exact path="/studyspacefinder" component={StudySpaceFinder}/>
       </Switch>
     </Router>
   );
