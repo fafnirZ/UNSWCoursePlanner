e4e7459fdc58d6b767206f519a4dfa89d523d159
MacBook Air <macbookair@Ahmets-Air.modem>
2021-03-30 00:26:55 +1100

    started front end implementation for studyBuddyFinder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from './pages/HomePage/homePage.js';
 import DashBoard from './pages/DashBoard/dashBoard.js';
 import StudySpaceFinder from './pages/StudySpaceFinderPage/studyFinder.js'
+import StudyBuddyFinder from './pages/StudyBuddyFinderPage/studyBuddyFinder.js'
 import './App.css';
 
 
@@ -12,7 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/dashboard" component={DashBoard} />
-        <Route exact path="/studyspacefinder" component={StudySpaceFinder}/>
+        <Route exact path="/studyspacefinder" component={StudySpaceFinder} />
+        <Route exact path="/studybuddyfinder" component={StudyBuddyFinder} />
       </Switch>
     </Router>
   );
--- src/pages/StudyBuddyFinderPage/studyBuddyFinder.js
+++ src/pages/StudyBuddyFinderPage/studyBuddyFinder.js
@@ -1,1 +1,17 @@
 import react from 'react'
+import Navbar from '../../components/Nav/nav.js'
+import './studyBuddyFinder.css'
+
+
+function StudyBuddyFinder() {
+    return (
+        <div className="studyBuddyFinder">
+            <div className="Header">
+				<Navbar />
+			</div>
+            
+        </div>
+    );    
+    // More to be done
+}
+export default StudyBuddyFinder;
