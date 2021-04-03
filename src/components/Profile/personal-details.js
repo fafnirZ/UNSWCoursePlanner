a53f3d94d0f48fad428c0158ab5962d5ff3591b4
MartinLe1 <martinle.au@gmail.com>
2021-04-03 15:38:14 +1100

    finished profile page

---
+++ src/components/Profile/personal-details.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './personal-details.css'
+
+function PersonalDetails () {
+    return (
+        <div className="details">
+            <div className="details-gender">
+                <div className="details-gender-title">
+                    <strong>Gender:</strong>
+                </div>
+                <div className="details-gender-user">
+                    Male
+                </div>
+            </div>
+            <div className="details-year">
+                <div className="details-year-title">
+                    <strong>Year:</strong>
+                </div>
+                <div className="details-year-user">
+                    4th Year
+                </div>
+            </div>
+            <div className="details-degree">
+                <div className="details-degree-title">
+                    <strong>Degree:</strong>
+                </div>
+                <div className="details-degree-user">
+                    3768 - Engineering(Honours)/Biomedical Engineering
+                </div>
+            </div>
+            <div className="details-email">
+                <div className="details-email-title">
+                    <strong>Email:</strong>
+                </div>
+                <div className="details-email-user">
+                    z5208859@ad.unsw.edu.au
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PersonalDetails;
