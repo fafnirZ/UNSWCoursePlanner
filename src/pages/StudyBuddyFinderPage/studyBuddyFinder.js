9aef3b11551eb230152e239748c4652871a78e8e
MacBook Air <macbookair@Ahmets-Air.modem>
2021-04-03 12:19:26 +1100

    playing around with style

--- src/pages/StudyBuddyFinderPage/studyBuddyFinder.js
+++ src/pages/StudyBuddyFinderPage/studyBuddyFinder.js
@@ -9,8 +9,25 @@ function StudyBuddyFinder() {
             <div className="Header">
 				<Navbar />
 			</div>
-            
+        
+            <div className="Body">
+                <div className="textbox">
+                    <h1>Textbox</h1>
+                </div>
+                <div className="Overview">
+                    <h1>Overview</h1>
+                </div>
+                <div className="Review">
+                    <h1>Review</h1>
+                </div>
+            </div>
+
+            <div className="Footer">
+                <h1>Smelly Footer</h1>
+            </div>
+
         </div>
+
     );    
     // More to be done
 }
