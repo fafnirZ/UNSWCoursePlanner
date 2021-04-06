5317e7943e0078e62500d1b470f374792d0f81b8
Zhilin Xie <jacky.xie@student.unsw.edu.au>
2021-04-06 16:26:59 +1000

    homepage fixed

--- src/pages/HomePage/homePage.js
+++ src/pages/HomePage/homePage.js
@@ -1,46 +1,23 @@
 import react from 'react'
-import StudyBuddy from '../../components/StudyBuddy/studyBuddy.js'
-import Dashboard from '../../pages/DashBoard/dashBoard.js'
-import StudySpaceFinder from '../../pages/StudySpaceFinderPage/studySpaceFinder.js'
 import {link} from 'react-router-dom';
 
 
-import './studyBuddyFinder.css'
 
 
-
-function HomePage() {
+export default function HomePage() {
 
     return (
         <div className="Home">
             <div className="Header">
-				<Navbar />
+                hi
 			</div>
         
             <div className="Body">
-                <div className="textbox">
-                    <h1>Home</h1>
-                </div>
-                <div className="StudyBuddyFinder">
-                    <Link to='/StudyBuddy'>StudyBuddy Finder</Link>
-                    <StudyBuddy />
-                </div>
-                <div className="StudySpaceFinder">
-                    <Link to='/StudyFinder'>Study Space Finder</Link>
-                    <StudySpaceFinder/>
-                </div>
-                <div className="reviewPage">
-                    <Link to='/reviewPage'>Reviews</Link>
-                    <ReviewPage/>
-                </div>
-                <div className="dashboard">
-                    <Link to='/dashBoard'>Dashboard</Link>
-                    <DashBoard/>
-                </div>
+                body
             </div>
 
             <div className="Footer">
-                <h1>Smelly Footer</h1>
+                hi
             </div>
 
         </div>
