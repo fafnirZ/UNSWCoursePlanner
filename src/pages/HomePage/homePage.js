cc1635369e789254080dc50f3990ca24677836dc
hsae123 <hsae123@outlook.com>
2021-04-06 12:25:00 +1000

    Update

--- src/pages/HomePage/homePage.js
+++ src/pages/HomePage/homePage.js
@@ -1,14 +1,49 @@
-import React, {useEffect} from 'react'
+import react from 'react'
+import StudyBuddy from '../../components/StudyBuddy/studyBuddy.js'
+import Dashboard from '../../pages/DashBoard/dashBoard.js'
+import StudySpaceFinder from '../../pages/StudySpaceFinderPage/studySpaceFinder.js'
+import {link} from 'react-router-dom';
 
 
-function HomePage(props) {
+import './studyBuddyFinder.css'
 
-	return (
-		<div>
-			TESTING THIS SHOULD DISPLAY
-		</div>
-	);
 
+
+function HomePage() {
+
+    return (
+        <div className="Home">
+            <div className="Header">
+				<Navbar />
+			</div>
+        
+            <div className="Body">
+                <div className="textbox">
+                    <h1>Home</h1>
+                </div>
+                <div className="StudyBuddyFinder">
+                    <Link to='/StudyBuddy'>StudyBuddy Finder</Link>
+                    <StudyBuddy />
+                </div>
+                <div className="StudySpaceFinder">
+                    <Link to='/StudyFinder'>Study Space Finder</Link>
+                    <StudySpaceFinder/>
+                </div>
+                <div className="reviewPage">
+                    <Link to='/reviewPage'>Reviews</Link>
+                    <ReviewPage/>
+                </div>
+                <div className="dashboard">
+                    <Link to='/dashBoard'>Dashboard</Link>
+                    <DashBoard/>
+                </div>
+            </div>
+
+            <div className="Footer">
+                <h1>Smelly Footer</h1>
+            </div>
+
+        </div>
+
+    );    
 }
-
-export default HomePage;
