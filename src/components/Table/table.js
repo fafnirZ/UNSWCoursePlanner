7009b8cb635010443941264508472436e14f898e
Zhilin Xie <jacky.xie@student.unsw.edu.au>
2021-04-13 16:35:34 +1000

    table is going great

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,60 +1,126 @@
-import react, {useEffect} from 'react'
+import react, { Component } from 'react'
 import './table.css'
 
-function Table (props) {
+class Table extends Component {
+	/*
+		data = {
+			years: [
+				{year: 1,
+				sem: 1,
+				courses: [
+					
+				]
+				}
+			]
+		}
+	*/
 
-	const num_years = 4;
-	const num_sems = 3;
+	constructor(props) {
+		super(props);
 
-	let Years = Array.from(Array(num_years).keys());
-	console.log(Years)				
+		this.state = {
+			num_years : 4,
+			num_sems : 3,	
 
+		};
 
-	return (
-		<table className="Table">
-			<li className="rows">
-				<div />
-				<div>
-					Semester 1
-				</div>
-				<div>
-					Semester 2
-				</div>
-				<div>
-					Semester 3
-				</div>
-			</li>
+		const yrs = Array.from(Array(this.state.num_years).keys());
+		const sems = Array.from(Array(this.state.num_sems).keys());
+		const default_course = Array.from(Array(3).keys());
 
-			{
-				Years.map((item, index) => {
-					console.log(item);
-					return (
-						<li className="rows">
-							{/*empty div*/}
-							<div>
-								Year {index+1}
-							</div>
-
-							<div>
-								
-							</div>
-							<div>
-								
-							</div>
-							<div>
-								
-							</div>
-						</li>
-					)
-
+		this.state.data = {
+				years : yrs.map((item, index) => {
+					return ({
+						year: item+1,
+						sems:
+							sems.map((item, index) => {
+								return ({
+									sem : item+1,
+									courses : 
+										default_course.map((item, index)=> {
+											return ({
+													index: item+1,
+													course: ""
+													
+												})
+										})
+							
+								})
+							})
+					})
 				})
-
-			}
-
+			};
 
 
-		</table>
-	);
+	}
+	componentDidMount() {
+
+
+	}
+
+
+
+	render() {
+	return (
+			<table className="Table">
+				<li className="rows">
+					<div />
+					<div>
+						Semester 1
+					</div>
+					<div>
+						Semester 2
+					</div>
+					<div>
+						Semester 3
+					</div>
+				</li>
+
+
+				{
+					this.state.data.years.map((item, index) => {
+						return (
+							<li className="rows">
+								<div>
+									Year {index+1}
+								</div>
+								{
+									item.sems.map((item, index) => {
+										return (
+												<div className="squares">
+													{
+														item.courses.map((item, index) => {
+
+															return (
+																<div className="items">
+																{item.course}
+																</div>
+															)
+														})
+													}
+												</div>
+											)
+									})
+								}
+							</li>
+						)
+
+					})
+
+				}
+
+
+
+
+			</table>
+		);
+
+
+
+
+	}
+
+
 
 
 }
