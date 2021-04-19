fd444bfac11d65ac0f5da31ca813049228f56972
Zhilin Xie <jacky.xie@student.unsw.edu.au>
2021-04-20 00:16:44 +1000

    progress

--- src/components/Profile/personal-details-edit.js
+++ src/components/Profile/personal-details-edit.js
@@ -4,51 +4,109 @@ import TextField from '@material-ui/core/TextField';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import react from 'react';
+import Menu from '@material-ui/core/menu';
+import MenuItem from '@material-ui/core/menu';
+
+
+import React from 'react';
 
 
 export default function EditProfile(props) {
-    const [open, setOpen] = react.useState(false);
+    const [open, setOpen] = React.useState(false);
     const handleClickOpen = () => {
     	setOpen(true);
   	};
 
-	const handleClose = () => {
+	const handleDialogClose = () => {
 		setOpen(false);
 	};
+
+	const handleDialogSubmit = () => {
+		setOpen(false);
+		
+	}
+
+
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+    	setAnchorEl(null);
+  	};
 
 	return(
 		[<button onClick={handleClickOpen}>
 			edit profile
 		</button>,
-		<Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+		<Dialog open={open} onClose={handleDialogClose} aria-labelledby="form-dialog-title">
             <DialogTitle id="form-dialog-title">
-                <strong>Create a Review</strong>
+                <strong>Edit profile</strong>
             </DialogTitle>
             <DialogContent>
+
+                 <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="email"
+                    type="email"
+                    variant="outlined"
+                    fullWidth
+                />
+                 <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Degree"
+                    type="degree"
+                    variant="outlined"
+                    fullWidth
+                />
+                 <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="year"
+                    type="year"
+                    variant="outlined"
+                />
+                 <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Gender"
+                    type="gender"
+                    variant="outlined"
+                />
+
                 <TextField
                     autoFocus
                     margin="dense"
                     id="name"
-                    label="Lecturer's Name"
-                    type="name"
+                    label="About me"
+                    type="about"
                     variant="outlined"
                     fullWidth
-                />
+                    multiline	
+                    rows={3}
+                /> 
                 <TextField
                     aria-label="empty textarea" 
-                    placeholder="Enter Review..." 
+                    placeholder="hobbies" 
+                    type="hobbies"
                     fullWidth 
                     multiline
+                    rows={5}
                     variant="outlined"
-                    rows={4}
+                
                 />
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose} color="primary">
+                <Button onClick={handleDialogClose} color="primary">
                      Cancel
                 </Button>
-                <Button onClick={handleClose} color="primary">
+                <Button onClick={handleDialogSubmit} color="primary">
                     Submit
                 </Button>
             </DialogActions>
