0db9322debb6b87188f451e8750746b3a622a25f
MacBook Air <macbookair@Ahmets-Air.modem>
2021-04-21 11:57:15 +1000

    adding changes to HomePage

--- src/pages/HomePage/homePage.js
+++ src/pages/HomePage/homePage.js
@@ -3,12 +3,10 @@ import Button from '@material-ui/core/Button'
 import './homePage.css'
 
 
-
-
 export default function HomePage() {
 
     return (
-        <div className="Home">
+        <div className="home">
             <div className="hHeader">
                 <div className="logo">
                     <img src="unswlogo.png"/>
@@ -18,14 +16,19 @@ export default function HomePage() {
                         login
                     </Button>
                 </div>
-			</div>
+            </div>
         
             <div className="hBody">
                 <div className="hBook">
                     <img src="book.png"/>
                 </div>
                 <div className="hWelcome">
-                    <img src="welcome.png"/>
+                <div className="semicircle">
+                    <div classname="writing">
+                        <span className="writing">Welcome to UNSW Course Planner</span>
+                    </div>
+                </div>
+                    
                 </div>
             </div>
 
